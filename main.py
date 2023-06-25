0f94a1389a98ff7fc68962c26637f179c7c79d60
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-06-25 16:55:47 +0400

    Move the `OAuth2Client` to a separate module

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from fastapi.templating import Jinja2Templates
 from social_core.backends.github import GithubOAuth2
 
 from demo.router import router as demo_router
+from fastapi_oauth2.client import OAuth2Client
 from fastapi_oauth2.middleware import OAuth2Middleware
 from fastapi_oauth2.router import router as oauth2_router
-from fastapi_oauth2.types import OAuth2Client
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -31,6 +31,7 @@ app.add_middleware(OAuth2Middleware, config={
             client_id="eccd08d6736b7999a32a",
             client_secret="642999c1c5f2b3df8b877afdc78252ef5b594d31",
             redirect_uri="http://127.0.0.1:8000/",
+            scope=["user:email"],
         ),
     ]
 })
