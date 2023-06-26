5f3d33a398af739bd5873bf71a399357e20c7eb9
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-06-26 14:19:49 +0400

    Replace the deprecated usage with new style

--- src/fastapi_oauth2/router.py
+++ src/fastapi_oauth2/router.py
@@ -2,31 +2,17 @@ from fastapi import APIRouter
 from fastapi.responses import RedirectResponse
 from starlette.requests import Request
 
-from fastapi_oauth2.github import GitHubOAuth2
-from .config import (
-    OAUTH2_CLIENT_ID,
-    OAUTH2_CLIENT_SECRET,
-    OAUTH2_CALLBACK_URL,
-)
-
 router = APIRouter(prefix="/oauth2")
-oauth2 = GitHubOAuth2(
-    client_id=OAUTH2_CLIENT_ID,
-    client_secret=OAUTH2_CLIENT_SECRET,
-    callback_url=OAUTH2_CALLBACK_URL,
-    allow_insecure_http=True,
-)
 
 
 @router.get("/{provider}/auth")
-async def login(provider: str):
-    print(provider)
-    return await oauth2.login_redirect()
+async def login(request: Request, provider: str):
+    return await request.auth.clients[provider].login_redirect(request)
 
 
-@router.get("/token")
-async def token(request: Request):
-    return await oauth2.token_redirect(request)
+@router.get("/{provider}/token")
+async def token(request: Request, provider: str):
+    return await request.auth.clients[provider].token_redirect(request)
 
 
 @router.get("/logout")
