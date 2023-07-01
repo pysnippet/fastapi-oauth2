42308d03ccf705b2ad703dc3d5982e48d55d1088
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-07-01 22:41:44 +0400

    GH-4: Move the demo app to `examples` dir

---
+++ examples/demonstration/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, APIRouter
+
+from config import oauth2_config
+from fastapi_oauth2.middleware import OAuth2Middleware
+from fastapi_oauth2.router import router as oauth2_router
+from router import router as app_router
+
+router = APIRouter()
+
+app = FastAPI()
+app.include_router(app_router)
+app.include_router(oauth2_router)
+app.add_middleware(OAuth2Middleware, config=oauth2_config)
