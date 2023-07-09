0e340596a2ecae29cb1734bada9a33b44812e800
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-07-09 22:35:29 +0400

    GH-5: Use `httpx.AsyncClient` for testing async routes

--- tests/test_oauth2_middleware.py
+++ tests/test_oauth2_middleware.py
@@ -1,17 +1,17 @@
-from fastapi import APIRouter
+import pytest
 from fastapi import FastAPI
-from fastapi.testclient import TestClient
+from httpx import AsyncClient
+from social_core.backends.github import GithubOAuth2
 
+from fastapi_oauth2.client import OAuth2Client
 from fastapi_oauth2.middleware import OAuth2Middleware
 from fastapi_oauth2.router import router as oauth2_router
 
 app = FastAPI()
-router = APIRouter()
 
 app.include_router(oauth2_router)
 app.add_middleware(OAuth2Middleware, config={})
 
-client = TestClient(app)
 
 
 def test_read_main():
