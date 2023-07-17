cc35c8f7ce43be92e6db16ee314da239e2853342
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-07-17 19:52:43 +0400

    Add an auth simulation

--- examples/demonstration/router.py
+++ examples/demonstration/router.py
@@ -6,6 +6,7 @@ from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
+from starlette.responses import RedirectResponse
 
 from database import get_db
 from fastapi_oauth2.security import OAuth2
@@ -19,6 +20,27 @@ templates = Jinja2Templates(directory="templates")
 @router.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "user": request.user, "json": json})
+
+
+@router.get("/auth")
+def sim_auth(request: Request):
+    access_token = request.auth.jwt_create({
+        "id": 0,
+        "avatar_url": None,
+        "name": "John Doe",
+        "email": "john.doe@auth.sim",
+        "login": "JohnDoe",
+        "exp": 3689609839,
+    })
+    response = RedirectResponse("/")
+    response.set_cookie(
+        "Authorization",
+        value=f"Bearer {access_token}",
+        max_age=request.auth.expires,
+        expires=request.auth.expires,
+        httponly=request.auth.http,
+    )
+    return response
 
 
 @router.get("/user")
