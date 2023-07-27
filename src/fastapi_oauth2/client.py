afcc936acabad566459c0e158e4082650b6c6e0c
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-07-27 23:55:57 +0400

    GH-9: Each client config can have its claim mappings

--- src/fastapi_oauth2/client.py
+++ src/fastapi_oauth2/client.py
@@ -1,8 +1,11 @@
 from typing import Optional
 from typing import Sequence
 from typing import Type
+from typing import Union
 
 from social_core.backends.oauth import BaseOAuth2
+
+from .claims import Claims
 
 
 class OAuth2Client:
@@ -13,6 +16,7 @@ class OAuth2Client:
     client_secret: str
     redirect_uri: Optional[str]
     scope: Optional[Sequence[str]]
+    claims: Optional[Union[Claims, dict]]
 
     def __init__(
             self,
@@ -22,9 +26,11 @@ class OAuth2Client:
             client_secret: str,
             redirect_uri: Optional[str] = None,
             scope: Optional[Sequence[str]] = None,
+            claims: Optional[Union[Claims, dict]] = None,
     ) -> None:
         self.backend = backend
         self.client_id = client_id
         self.client_secret = client_secret
         self.redirect_uri = redirect_uri
         self.scope = scope or []
+        self.claims = Claims(claims)
