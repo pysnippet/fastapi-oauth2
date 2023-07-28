6cdfddccc42bdd82d81e5bbb65428241bc874b95
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-07-28 18:36:02 +0400

    GH-9: Fix init when `Claims` obtains `Claims` as a value

--- src/fastapi_oauth2/claims.py
+++ src/fastapi_oauth2/claims.py
@@ -13,50 +13,7 @@ class Claims(dict):
 
     def __init__(self, seq=None, **kwargs) -> None:
         super().__init__(seq or {}, **kwargs)
-        self.display_name = self.get("display_name", "name")
-        self.identity = self.get("identity", "sub")
-        self.picture = self.get("picture", "picture")
-        self.email = self.get("email", "email")
-
-    # @property
-    # def display_name(self) -> str:
-    #     return self.get("display_name", "")
-    #
-    # @display_name.setter
-    # def display_name(self, value: Union[Any, Callable[[dict], Any]]) -> None:
-    #     self["display_name"] = self._get_value(value)
-    #
-    # @property
-    # def identity(self) -> str:
-    #     return self.get("identity", "")
-    #
-    # @identity.setter
-    # def identity(self, value: Union[str, Callable[[dict], Any]]) -> None:
-    #     self["identity"] = self._get_value(value)
-    #
-    # @property
-    # def picture(self) -> str:
-    #     return self.get("picture", "")
-    #
-    # @picture.setter
-    # def picture(self, value: Union[str, Callable[[dict], Any]]) -> None:
-    #     self["picture"] = self._get_value(value)
-    #
-    # @property
-    # def email(self) -> str:
-    #     return self.get("email", "")
-    #
-    # @email.setter
-    # def email(self, value: Union[str, Callable[[dict], Any]]) -> None:
-    #     self["email"] = self._get_value(value)
-
-    def __getattr__(self, item):
-        attr = super().get(item)
-        if callable(attr):
-            return attr(self)
-        return self.get(attr)
-
-    # def _get_value(self, value: Union[str, Callable[[dict], Any]]) -> Any:
-    #     if callable(value):
-    #         return value(self)
-    #     return self.get(value, "")
+        self["display_name"] = kwargs.get("display_name", self.get("display_name", "name"))
+        self["identity"] = kwargs.get("identity", self.get("identity", "sub"))
+        self["picture"] = kwargs.get("picture", self.get("picture", "picture"))
+        self["email"] = kwargs.get("email", self.get("email", "email"))
