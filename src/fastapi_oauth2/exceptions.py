23658d3e014c21a4baad049ddf82d259f3f33392
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-10-06 22:19:31 +0400

    Define the initial set of exceptions

--- src/fastapi_oauth2/exceptions.py
+++ src/fastapi_oauth2/exceptions.py
@@ -1,5 +1,17 @@
 from starlette.exceptions import HTTPException
 
 
-class OAuth2LoginError(HTTPException):
-    """Raised when any login-related error occurs."""
+class OAuth2Error(HTTPException):
+    ...
+
+
+class OAuth2AuthenticationError(OAuth2Error):
+    ...
+
+
+class OAuth2BadCredentialsError(OAuth2Error):
+    ...
+
+
+class OAuth2InvalidRequestError(OAuth2Error):
+    ...
