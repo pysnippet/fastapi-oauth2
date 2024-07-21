6dbfd40187961bbbaf2f5d69a7ff94c37b2df50d
Ilya Borowski <40835268+Scurrra@users.noreply.github.com>
2024-07-21 16:40:19 +0300

    Update src/fastapi_oauth2/middleware.py
    Co-authored-by: Artyom Vancyan <44609997+ArtyomVancyan@users.noreply.github.com>

--- src/fastapi_oauth2/middleware.py
+++ src/fastapi_oauth2/middleware.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from datetime import timedelta
-from datetime import UTC
+from datetime import timezone
 from typing import Any
 from typing import Awaitable
 from typing import Callable
