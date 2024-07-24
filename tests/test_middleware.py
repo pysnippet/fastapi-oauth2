1cef8b8f74d004fef9fa8398cf92ee74fb138902
David García Garzón <david.garcia@somenergia.coop>
2024-07-24 20:36:14 +0200

    GH-42: testscases middleware overhandling excepts
    - Unintended errors in code should be 500
    - Non Http custom exceptions should be 500
    - Handled exceptions should use handler (and be 418 in this case)
    
    But all of them are reported as 401 Unauthorized even
    when the entrypoint does not requires authorization.

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -26,3 +26,45 @@ async def test_middleware_on_logout(get_app):
 
         response = await client.get("/user")
         assert response.status_code == 403  # Forbidden
+
+@pytest.mark.anyio
+async def test_middleware_do_not_interfer_user_errors(get_app):
+    app=get_app()
+    @app.get('/unexpected_error')
+    def unexpected():
+        undefined_id # Intended code error
+
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.get("/unexpected_error")
+        assert response.status_code == 500  # Internal server error
+
+@pytest.mark.anyio
+async def test_middleware_ignores_custom_exceptions(get_app):
+    class MyCustomException(Exception): pass
+    app=get_app()
+    @app.get('/custom_exception')
+    def custom_exception():
+        raise MyCustomException()
+
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.get("/custom_exception")
+        assert response.status_code == 500  # Internal server error
+
+@pytest.mark.anyio
+async def test_middleware_ignores_handled_custom_exceptions(get_app):
+    class MyCustomException(Exception): pass
+    app=get_app()
+    @app.exception_handler(MyCustomException)
+    async def unicorn_exception_handler(request, exc):
+        return JSONResponse(
+            status_code=418,
+            content={"message": f"I am a Teapot!"},
+        )
+
+    @app.get('/custom_exception')
+    def custom_exception():
+        raise MyCustomException()
+
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.get("/custom_exception")
+        assert response.status_code == 418 # I am a teapot!
