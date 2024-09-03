a057c048aefc7dd1cec07a6c5b5fac5256a6b275
Tatayoyoh <r.dubodan@gmail.com>
2024-09-03 19:12:54 +0200

    Fix authorization_url and access_token_url variables resolution (like facebook backend)

--- src/fastapi_oauth2/core.py
+++ src/fastapi_oauth2/core.py
@@ -1,3 +1,4 @@
+import os
 import json
 import random
 import re
@@ -33,7 +34,8 @@ class OAuth2Strategy(BaseStrategy):
         return path
 
     def get_setting(self, name) -> Any:
-        """Mocked setting method."""
+        """ settings from environment """
+        return os.getenv(name, '')
 
     @staticmethod
     def get_json(url, method='GET', *args, **kwargs) -> httpx.Response:
@@ -64,8 +66,8 @@ class OAuth2Core:
         self.provider = client.backend.name
         self.redirect_uri = client.redirect_uri
         self.backend = client.backend(OAuth2Strategy())
-        self._authorization_endpoint = client.backend.AUTHORIZATION_URL
-        self._token_endpoint = client.backend.ACCESS_TOKEN_URL
+        self._authorization_endpoint = self.backend.authorization_url()
+        self._token_endpoint = self.backend.access_token_url()
         self._oauth_client = WebApplicationClient(self.client_id)
 
     @property
