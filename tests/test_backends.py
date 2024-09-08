23e8a66947da3ac7ed9701d22f773e5468dffc97
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2024-09-08 15:19:46 +0400

    Fix tests for latest versions of FastAPI

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -9,6 +9,7 @@ from fastapi_oauth2.core import OAuth2Core
 @pytest.mark.anyio
 async def test_core_init_with_all_backends(backends):
     os.environ["OIDC_ENDPOINT"] = "https://oidctest.wsweet.org"
+    os.environ["BASE_URL"] = "https://oidctest.wsweet.org"
 
     for backend in backends:
         try:
